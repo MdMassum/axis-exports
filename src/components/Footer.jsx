9e991d0793ed6c7d3f92416f61c2e40dfddde753
Md Emamudin <mdemamudin726@gmail.com>
2025-03-19 11:26:35 +0530

    updated contacts and enhanced footer design

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,43 +1,121 @@
 import { HashLink } from "react-router-hash-link";
+import {
+  FaMapMarkerAlt,
+  FaEnvelope,
+  FaPhone,
+  FaExternalLinkAlt,
+  FaFacebook,
+  FaTwitter,
+  FaInstagram,
+} from "react-icons/fa";
+import { MdOutlineCategory, MdLogin } from "react-icons/md";
+import { HiOutlineHome, HiOutlineMail } from "react-icons/hi";
+import skillancer from "../assets/skillancer.png";
 
 export default function Footer() {
   const quickLinks = [
-    { label: "HOME", href: "#home" },
-    { label: "CATEGORY", href: "#category" },
-    { label: "ABOUT US", href: "#aboutUs" },
-    { label: "ENQUIRY", href: "/enquiry" },
-    { label: "ADMIN LOGIN", href: "/admin/login" },
+    { label: "HOME", href: "#home", icon: <HiOutlineHome /> },
+    { label: "CATEGORY", href: "#category", icon: <MdOutlineCategory /> },
+    { label: "ABOUT US", href: "#aboutUs", icon: <FaMapMarkerAlt /> },
+    { label: "ENQUIRY", href: "/enquiry", icon: <HiOutlineMail /> },
+    { label: "ADMIN LOGIN", href: "/admin/login", icon: <MdLogin /> },
   ];
 
   return (
-    <footer className="bg-[#278800] text-white py-6 px-5 md:px-24">
-      <div className="flex md:flex-row flex-col md:gap-16 gap-6">
+    <footer className="bg-gradient-to-r from-green-600 to-green-800 text-white py-10 px-5 md:px-12 lg:px-24">
+      <div className="grid md:grid-cols-3 md:gap-48 lg:gap-56">
         {/* First Column - Address */}
-        <div className="flex flex-col">
-          <h3 className="font-bold text-lg">Address</h3>
-          <p className="text-sm mt-2">123 Street Name, City, Country</p>
-          <p className="text-sm">Email: example@email.com</p>
-          <p className="text-sm">Phone: +123 456 7890</p>
+        <div className="flex flex-col space-y-2 md:min-w-88 lg:min-w-96">
+          <h3 className="font-bold text-2xl">Contact Us</h3>
+          <div className="flex items-center space-x-2">
+            <FaMapMarkerAlt className="text-xl" />
+            <div>
+              <p className="text-sm">
+                Shop No 8 (1st floor), Gayatri Market, Jhokan Bagh,
+              </p>
+              <p className="text-sm">Jhansi, UP, India 284001</p>
+            </div>
+          </div>
+          <div className="flex items-center space-x-2">
+            <FaEnvelope className="text-xl" />
+            <p className="text-sm">info@axisinternationalexports.com</p>
+          </div>
+          <div className="flex items-center space-x-2 mt-1 md:mt-2">
+            <FaPhone className="text-xl" />
+            <p className="text-sm">+91 7985538177</p>
+          </div>
         </div>
 
         {/* Second Column - Quick Links */}
-        <div className="flex flex-col">
-          <h3 className="font-bold text-lg">Quick Links</h3>
-          <ul className="mt-2 ">
+        <div className="flex flex-col md:ml-10 mt-8 md:mt-0">
+          <h3 className="font-bold text-2xl min-w-36">Quick Links</h3>
+          <ul className="mt-2 space-y-1">
             {quickLinks.map((link, index) => (
-              <li key={index}>
+              <li key={index} className="flex items-center space-x-2 min-w-36">
                 {link.href.startsWith("#") ? (
-                  <HashLink smooth to={`/${link.href}`} className="hover:underline">
-                    {link.label}
+                  <HashLink
+                    smooth
+                    to={`/${link.href}`}
+                    className="hover:underline flex items-center space-x-2 transition duration-300 hover:text-gray-300"
+                  >
+                    {link.icon}
+                    <span>{link.label}</span>
                   </HashLink>
                 ) : (
-                  <a href={link.href} className="hover:underline">
-                    {link.label}
+                  <a
+                    href={link.href}
+                    className="hover:underline flex items-center space-x-2 transition duration-300 hover:text-gray-300"
+                  >
+                    {link.icon}
+                    <span>{link.label}</span>
                   </a>
                 )}
               </li>
             ))}
           </ul>
+        </div>
+
+        {/* Third Column - Developer Credit and Social Media */}
+        <div className="flex flex-col items-start mt-8 md:mt-0">
+          <h3 className="font-bold text-2xl min-w-36">Follow Us</h3>
+          <div className="flex space-x-4 mt-2">
+            <a
+              href="https://www.facebook.com/people/Axis-International-Exports/61573413671024/"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="hover:text-gray-300"
+            >
+              <FaFacebook className="text-xl" />
+            </a>
+            <a
+              href="https://www.instagram.com/axis_international_exports/"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="hover:text-gray-300"
+            >
+              <FaTwitter className="text-xl" />
+            </a>
+            <a
+              href="https://www.instagram.com/axis_international_exports/"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="hover:text-gray-300"
+            >
+              <FaInstagram className="text-xl" />
+            </a>
+          </div>
+
+          <a
+            href="https://skillancer.in/"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="flex flex-col items-start cursor-pointer space-y-2 mt-8 md:mt-6"
+          >
+            <p className="flex items-center space-x-2 min-w-36">
+              Developed By <FaExternalLinkAlt className="text-sm ml-1" />
+            </p>
+            <img src={skillancer} alt="Skillancer Logo" className="h-6" />
+          </a>
         </div>
       </div>
     </footer>
--- src/pages/Home/components/Contact.jsx
+++ src/pages/Home/components/Contact.jsx
@@ -1,4 +1,4 @@
-import { FaInstagram, FaFacebookF, FaWhatsapp, FaPhone} from "react-icons/fa";
+import { FaInstagram, FaFacebookF, FaWhatsapp, FaPhone } from "react-icons/fa";
 import { MdAttachEmail } from "react-icons/md";
 
 import logo from "../../../assets/logo.jpg";
@@ -6,7 +6,10 @@ import leaves from "../../../assets/multipleLeaf.png";
 
 const Contact = () => {
   return (
-    <div id="contactUs" className="w-full md:w-[85%] flex flex-col md:flex-row mx-auto p-5 md:p-0  justify-between md:gap-16 gap-8 md:my-10 md:py-6 bg-white rounded-lg ">
+    <div
+      id="contactUs"
+      className="w-full md:w-[85%] flex flex-col md:flex-row mx-auto p-5 md:p-0  justify-between md:gap-16 gap-8 md:my-10 md:py-6 bg-white rounded-lg "
+    >
       {/* Logo Section */}
       <div className="border-2 border-blue-300 p-6 rounded-lg flex items-center justify-center md:w-1/2">
         <img src={logo} alt="Company Logo" className="w-[480px]" />
@@ -15,35 +18,72 @@ const Contact = () => {
       {/* Contact Section */}
       <div className="flex flex-col justify-between  w-full md:w-1/2 text-green-700 text-left">
         <div className="w-full flex items-center mb-7 md:mb-16">
-          <div className="flex flex-row md:flex-col items-center justify-center md:justify-start gap-5 md:gap-0 font-bold md:-ml-6 md:mt-2" style={{fontFamily:'Nunito', fontWeight: 999}}>
-            <h2 className="text-6xl md:text-8xl text-left min-w-56 md:-ml-9">Get in</h2>
-            <h2 className="text-lime-500 text-6xl md:-mt-4 md:text-8xl -ml-14 md:-ml-1">touch!</h2>
+          <div
+            className="flex flex-row md:flex-col items-center justify-center md:justify-start gap-5 md:gap-0 font-bold md:-ml-6 md:mt-2"
+            style={{ fontFamily: "Nunito", fontWeight: 999 }}
+          >
+            <h2 className="text-6xl md:text-8xl text-left min-w-56 md:-ml-9">
+              Get in
+            </h2>
+            <h2 className="text-lime-500 text-6xl md:-mt-4 md:text-8xl -ml-14 md:-ml-1">
+              touch!
+            </h2>
           </div>
 
-          <img src={leaves} alt="" className="absolute right-4 mb-6 mr-4 hidden lg:flex w-96" />
+          <img
+            src={leaves}
+            alt=""
+            className="absolute right-4 mb-6 mr-4 hidden lg:flex w-96"
+          />
         </div>
 
         <div className="space-y-2 md:space-y-3 md:-ml-4">
-          <div className="flex items-center space-x-4">
+          <a
+            href="https://www.instagram.com/axis_international_exports/"
+            target="_blank"
+            className="flex items-center space-x-4 cursor-pointer"
+          >
             <FaInstagram className="text-pink-500 text-2xl md:text-4xl" />
-            <span className="font-bold text-lg md:text-2xl">@instagram</span>
-          </div>
-          <div className="flex items-center space-x-4">
+            <span className="font-bold text-lg md:text-2xl">
+              axis_international_exports
+            </span>
+          </a>
+          <a
+            href="https://www.facebook.com/people/Axis-International-Exports/61573413671024/"
+            target="_blank"
+            className="flex items-center space-x-4 cursor-pointer"
+          >
             <FaFacebookF className="text-blue-500 text-2xl md:text-4xl" />
-            <span className="font-bold text-lg md:text-2xl">@facebook</span>
-          </div>
-          <div className="flex items-center space-x-4 ">
+            <span className="font-bold text-lg md:text-2xl">
+              Axis International Exports
+            </span>
+          </a>
+          <a
+            href="https://wa.me/917985538177"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="flex items-center space-x-4 cursor-pointer"
+          >
             <FaWhatsapp className="text-green-500 text-2xl md:text-4xl" />
-            <span className="font-bold text-lg md:text-2xl">123 456 7890</span>
-          </div>
-          <div className="flex items-center space-x-4 ">
+            <span className="font-bold text-lg md:text-2xl">
+              +91 7985538177
+            </span>
+          </a>
+          <a
+            href="mailto:info@axisinternationalexports.com"
+            className="flex items-center space-x-4 cursor-pointer"
+          >
             <MdAttachEmail className="text-blue-500  text-2xl md:text-4xl" />
-            <span className="font-bold text-lg md:text-2xl">abc@email.com</span>
-          </div>
-          <div className="flex items-center space-x-5 mb-6">
+            <span className="font-bold text-lg md:text-2xl">
+              info@axisinternationalexports.com
+            </span>
+          </a>
+          <a href="tel:+917985538177" className="flex items-center space-x-5 mb-6 cursor-pointer">
             <FaPhone className="text-gray-500 text-2xl md:text-3xl" />
-            <span className="font-bold text-lg md:text-2xl">123 456 7890</span>
-          </div>
+            <span className="font-bold text-lg md:text-2xl">
+              +91 7985538177
+            </span>
+          </a>
         </div>
       </div>
     </div>
